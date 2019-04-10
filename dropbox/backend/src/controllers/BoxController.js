b8707bed6e33d159b2018ea69a02b243283eb92b
Rodrigo Passos <rodrigo.passos25@gmail.com>
2019-04-10 00:49:33 -0300

    Deploy para o Heroku

---
+++ dropbox/backend/src/controllers/BoxController.js
@@ -0,0 +1,27 @@
+const Box = require('./../models/Box')
+
+class BoxController {
+    async show(req, res) {
+        const box = await Box.findById(req.params.id).populate({
+            path: 'files',
+            options: {
+                sort: {
+                    createdAt: -1
+                }
+            }
+        })
+
+        return res.json(box)
+    }
+
+    async store(req, res) {
+        const { title } = req.body
+        const box = await Box.create({
+            title
+        })
+
+        return res.json(box)
+    }
+}
+
+module.exports = new BoxController()
