702010e9fe7c34c9dff677582d5aafefc4c97a3e
Rodrigo Passos <rodrigo.passos25@gmail.com>
2019-04-12 00:32:22 -0300

    Adicionado app mobile

---
+++ dropbox/mobile/src/routes.js
@@ -0,0 +1,13 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+
+import Box from './pages/Box'
+import Main from './pages/Main'
+
+const Routes = createAppContainer(
+    createSwitchNavigator({
+        Main,
+        Box
+    })
+)
+
+export default Routes
